3f6cf922abeb23b5de34883c3e940016713b9897
Kenny <kennyodb@hotmail.com>
2014-05-21 19:04:11 +0200

    some scheduling

--- src/company/schedule/SchedulingAlgorithm.java
+++ src/company/schedule/SchedulingAlgorithm.java
@@ -1,140 +1,112 @@
 package company.schedule;
 
-//import java.util.Collections;
-//import java.util.Comparator;
-//import java.util.LinkedList;
-//import java.util.List;
-//import java.util.Set;
-//import java.util.SortedMap;
-//
-//import util.Timestamp;
-//import vehicle.order.Order;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import vehicle.order.Order;
+
+import company.assemblylines.Assemblyline;
+
+/**
+ * Represents a scheduling algorithm. Before using scheduling methods, users
+ * must make sure to set the assemblylines before calling scheduling methods.
+ */
 public abstract class SchedulingAlgorithm {
 
-//	protected List<Order> orders;
-//	protected List<Order> inAssembly;
-//	protected Timestamp time;
-//
-//	/**
-//	 * schedules the given list of orders. This method will not alter its
-//	 * parameters.
-//	 * 
-//	 * @param currentTime
-//	 *            The current time.
-//	 * @param pending
-//	 *            The pending orders that need to be scheduled.
-//	 * @param inAssembly
-//	 *            The orders that are currently being assembled on the
-//	 *            assemblyLine. The first order in this list should be
-//	 *            associated with the first workstation.
-//	 * @return A sorted map, containing all the orders contained by the given
-//	 *         list of pending orders, sorted in the order they are scheduled.
-//	 *         The keys in this map are the times that the corresponding orders
-//	 *         (values) are scheduled to be completed. The result includes the
-//	 *         orders that are currently in assembly.
-//	 */
-//	public abstract SortedMap<Timestamp, Order> schedule(Timestamp currentTime,
-//			List<Order> pending, List<Order> inAssembly);
-//
-//	/**
-//	 * returns the first order for a new day, if any. This is calculated in such
-//	 * way that the amount of idle workstations at the start of the day is
-//	 * minimal. The order will therefore be an order for which there is no work
-//	 * in the first workstation. This method applies FIFO in finding fitting
-//	 * orders and returns the n-th one.
-//	 * 
-//	 * @param n
-//	 *            if n is 0, returns the first order that meets the requirement.
-//	 *            If n is 1, the second one is returned, and so on
-//	 * 
-//	 * @return the n-th first order for a new day or null if no such order
-//	 *         exists.
-//	 */
-//	protected Order getFirstOrder(int n) {
-//		int ordersFound = 0;
-//		for (Order order : orders) {
-//			Set<Integer> ws = order.getNeededWorkstations();
-//			if (ws.size() < inAssembly.size() && !ws.contains(0)) {
-//				if (++ordersFound > n) {
-//					return order;
-//				}
-//			}
-//		}
-//		return null;
-//	}
-//
-//	/**
-//	 * returns the last order for an ending day, if any. This is calculated in
-//	 * such way that the amount of idle workstations at the ending of the day is
-//	 * minimal. The order will therefore be an order for which there is no work
-//	 * in the last workstation. This method applies FIFO in finding fitting
-//	 * orders and returns the n-th one.
-//	 * 
-//	 * @param n
-//	 *            if n is 0, returns the first order that meets the requirement.
-//	 *            If n is 1, the second one is returned, and so on
-//	 * 
-//	 * @return the (n+1)th first order for a new day or null if no such order
-//	 *         exists.
-//	 */
-//	protected Order getLastOrder(int n) {
-//		int ordersFound = 0;
-//		for (Order order : orders) {
-//			Set<Integer> ws = order.getNeededWorkstations();
-//			if (ws.size() < inAssembly.size()
-//					&& !ws.contains(inAssembly.size())) {
-//				if (++ordersFound > n) {
-//					return order;
-//				}
-//			}
-//		}
-//		return null;
-//	}
-//
-//	/**
-//	 * @return A list containing all pending orders with a deadline, sorted by
-//	 *         their deadlines.
-//	 */
-//	protected List<Order> getDeadlines() {
-//		List<Order> result = new LinkedList<>();
-//		for (Order order : orders) {
-//			if (order.getDeadline() != null) {
-//				result.add(order);
-//			}
-//		}
-//		Collections.sort(result, new Comparator<Order>() {
-//			@Override
-//			public int compare(Order o1, Order o2) {
-//				return o1.getDeadline().compareTo(o2.getDeadline());
-//			}
-//		});
-//		return result;
-//	}
-//
-//	protected int getEstimatedCycleTime() {
-//		int result = 0;
-//		for (Order order : inAssembly) {
-//			if (order.getBuildingTimeFor() > result) {
-//				result = order.getBuildingTimeFor();
-//			}
-//		}
-//		return 0;
-//	}
-//
-//	protected Order addToAssemblyList(Order next) {
-//		inAssembly.add(0, next);
-//		return inAssembly.remove(inAssembly.size() - 1);
-//	}
-//
-//	protected boolean nextIsToday(Order next) {
-//		LinkedList<Order> assembly = new LinkedList<>(inAssembly);
-//		addToAssemblyList(next);
-//		Timestamp nextTime = time.increaseTime(getEstimatedCycleTime());
-//		while (addToAssemblyList(null) != next) {
-//			nextTime = nextTime.increaseTime(getEstimatedCycleTime());
-//		}
-//		inAssembly = assembly; // undo changes
-//		return !nextTime.shouldBeFinished();
-//	}
+	private List<Order> pending = Collections.emptyList();
+	private Map<Assemblyline, List<Order>> schedule = Collections.emptyMap();
+
+	/**
+	 * Clones the given assemblylines all individually and keeps the clones for
+	 * scheduling.
+	 */
+	public void setAssemblylines(Set<Assemblyline> argLines) {
+		if (argLines == null) {
+			throw new IllegalArgumentException("invalid set of assemblylines");
+		}
+		this.schedule = new HashMap<>();
+		for (Assemblyline argLine : argLines) {
+			this.schedule.put(argLine.clone(), new LinkedList<Order>());
+		}
+	}
+
+	/**
+	 * schedules all given orders over the assemblylines that are currently set
+	 * for this algorithm. If an order in the pending list cannot be scheduled,
+	 * this order will simply be ignored.
+	 * 
+	 * @param pending
+	 *            the orders that will be scheduled
+	 * @return A map containing the assemblylines for which orders were
+	 *         scheduled as keys, and their respective lists of scheduled orders
+	 *         as values.
+	 */
+	public Map<Assemblyline, List<Order>> schedule(List<Order> pending) {
+		this.resetSchedule();
+		this.pending = new LinkedList<>(pending);
+		while (scheduleNext()) {
+			;
+		}
+		return new HashMap<>(schedule);
+	}
+
+	/**
+	 * Keeps the assemblylines, but sets all associated lists of orders to new,
+	 * empty lists.
+	 */
+	private void resetSchedule() {
+		Set<Assemblyline> keys = schedule.keySet();
+		schedule = new HashMap<>();
+		for (Assemblyline key : keys) {
+			schedule.put(key, new LinkedList<Order>());
+		}
+	}
+
+	/**
+	 * @return A set containing all assemblylines for which this algorithm is
+	 *         currently scheduling.
+	 */
+	protected Set<Assemblyline> getAssemblylines() {
+		return new HashSet<>(schedule.keySet());
+	}
+
+	/**
+	 * @return A copy of the list containing all orders that have not (yet) been
+	 *         scheduled.
+	 */
+	protected List<Order> getPending() {
+		return new LinkedList<>(pending);
+	}
+
+	/**
+	 * Adds the given order to the schedule associated with the given
+	 * assemblyline and removes it from the pending orders.
+	 * 
+	 * @return true if the schedule was changed by this method. Returns false if
+	 *         the given order was not contained in the list of pending orders,
+	 *         or if the given assemblyline is not a key in the schedule.
+	 */
+	protected boolean addToSchedule(Order order, Assemblyline assemblyline) {
+		if (!pending.contains(order)
+				|| !schedule.keySet().contains(assemblyline)) {
+			return false;
+		}
+		pending.remove(order);
+		schedule.get(assemblyline).add(order);
+		return true;
+	}
+
+	/**
+	 * performs one step in the scheduling process. The implementing method
+	 * should end with, and return the call to the addToSchedule() method of
+	 * this class.
+	 * 
+	 * @return true if the schedule was changed by calling this method.
+	 */
+	protected abstract boolean scheduleNext();
 }
