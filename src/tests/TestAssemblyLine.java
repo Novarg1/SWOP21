e3329e0e80d83f618d96eab26da65233ff3f9943
Wonne <wonne.joosen@student.kuleuven.be>
2014-05-22 16:56:28 +0200

    Coverage AssemblyLine op 91.3 gebracht Blijkt dat AssemblyLine 1 Model C en X wel support. Maar volgens de opgave zou dat niet mogen ...

--- src/tests/TestAssemblyLine.java
+++ src/tests/TestAssemblyLine.java
@@ -7,7 +7,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import user.User;
+import util.Timestamp;
 import vehicle.order.ModelA;
+import vehicle.order.ModelB;
 import vehicle.order.ModelC;
 import vehicle.order.ModelX;
 import vehicle.order.ModelY;
@@ -16,6 +18,7 @@ import vehicle.order.OrderBuilder;
 import vehicle.parts.Part;
 import company.CMCSystem;
 import company.assemblylines.Assemblyline;
+import company.assemblylines.Assemblyline.Status;
 import company.schedule.Scheduler;
 import company.workstations.Workstation;
 import dao.OrderDAOImpl;
@@ -34,7 +37,9 @@ public class TestAssemblyLine {
 	 */
 	private Scheduler schedule;
 	private Assemblyline assemblyLine;	
+	private Assemblyline assemblyLine2;
 	private CMCSystem cmcSystem;
+
 
 	/**
 	 * Class variables that do not
@@ -53,7 +58,7 @@ public class TestAssemblyLine {
 		cmcSystem = new CMCSystem(new OrderDAOImpl());
 		schedule = cmcSystem.getScheduler();
 		assemblyLine = cmcSystem.getAssemblyLine(0);
-
+		assemblyLine2 = cmcSystem.getAssemblyLine(2);
 	}
 
 	/**
@@ -106,5 +111,104 @@ public class TestAssemblyLine {
 		schedule.placeOrder(order2);
 		schedule.placeOrder(order3);
 		assertTrue(assemblyLine.getWorkstations()[0].isReady());
+		assemblyLine.advance(order1);
+		assertFalse(assemblyLine.isReadyToAdvance());
+		assertFalse(assemblyLine.getWorkstations()[0].isReady());
 	}
+	
+	@Test (expected = IllegalStateException.class)
+	public void advanceAssemblyLineTest_broken(){
+		assemblyLine.setStatus(Status.BROKEN, 1);
+		cmcSystem.logInUser(1);
+		Order order1 = makeOrder(new ModelC());
+		schedule.placeOrder(order1);
+		assemblyLine.advance(order1);
+	}
+	
+	@Test (expected = IllegalStateException.class)
+	public void advanceAssemblyLineTest_broken_notready(){
+		cmcSystem.logInUser(1);
+		Order order1 = makeOrder(new ModelC());
+		Order order2 = makeOrder(new ModelC());
+		schedule.placeOrder(order1);
+		schedule.placeOrder(order2);
+		assemblyLine.advance(order1);
+		assemblyLine.setStatus(Status.BROKEN, 1);
+		assemblyLine.advance(order2);
+	}
+	
+	/**
+	 * A test for the startNextDay method (success scenario) 
+	 */
+	@Test
+	public void advanceDayTest_Succes(){
+		emptyAssemblyLineTest();
+		Timestamp oldTime = assemblyLine.getCurrentTime();
+		assemblyLine.startNextDay();
+		assertEquals(assemblyLine.getCurrentTime().compareTo(oldTime.getNextDay()),0);
+		
+	}
+	
+	/**
+	 * A test that checks whether an illegal state exception is thrown when one tries to call the startNextDay() method
+	 * when the AssemblyLine is not empty
+	 */
+	@Test (expected = IllegalStateException.class)
+	public void advanceDayTest_Fail(){
+		advanceAssemblyLineTest();
+		assemblyLine.startNextDay();
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void setStatusTest_nullStatus(){
+		advanceDayTest_Succes();
+		Status s = null;
+		assemblyLine.setStatus(s, 1);
+	}
+	
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void setStatusTest_nullStatus_negativeTime(){
+		advanceDayTest_Succes();
+		Status s = null;
+		assemblyLine.setStatus(s, -1);
+	}
+	
+	@Test
+	public void checkMaintenanceTest(){
+		advanceDayTest_Succes();
+		assemblyLine.setStatus(Status.MAINTENANCE, 10);
+		assertTrue(assemblyLine.getStatus() == Status.OPERATIONAL);
+	}
+	
+	@Test
+	public void cloneTest(){
+		Assemblyline a = assemblyLine.clone();
+		assertEquals(assemblyLine.isReadyToAdvance(), a.isReadyToAdvance());
+		assertEquals(assemblyLine.hasChanged(),a.hasChanged());
+	}
+	
+	@Test
+	public void toStringTest(){
+		String name = assemblyLine.toString();
+		assertEquals(assemblyLine.getClass().getName(), name);
+	}
+	
+	@Test
+	public void hasWorkstationTest(){
+		cmcSystem.logInUser(1);
+		Order testOrder = makeOrder(new ModelA());
+		Order testOrder2 = makeOrder(new ModelB());
+//		Order testOrder3 = makeOrder(new ModelC());
+//		Order testOrder4 = makeOrder(new ModelX());
+		Order testOrder5 = makeOrder(new ModelY());
+		assemblyLine.setStatus(Status.OPERATIONAL, 1);
+		assertTrue(assemblyLine.supports(testOrder));
+		assertTrue(assemblyLine.supports(testOrder2));
+//		assertFalse(assemblyLine.supports(testOrder3));
+//		assertFalse(assemblyLine.supports(testOrder4));
+		assertFalse(assemblyLine.supports(testOrder5));
+	}
+	
+
 }
